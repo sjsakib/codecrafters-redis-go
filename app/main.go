5f0242c0fc240451ef05b8faa440468f31f16a7b
Sakib <sjsakib.bd@gmail.com>
2025-12-23 00:33:14 +0600

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -15,9 +15,40 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	// defer conn.Close()
+
+	_, err = conn.Write([]byte("+PONG\r\n"))
+	if err != nil {
+		fmt.Println("Error writing to connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	// buf := make([]byte, 1024)
+	// n, err := conn.Read(buf)
+
+	// if err != nil {
+	// 	fmt.Println("Error reading from connection: ", err.Error())
+	// 	os.Exit(1)
+	// }
+
+	// fmt.Println("Received data: ", string(buf[:n]) )
+
+	// if string(buf[:n]) == "PING\r\n" {
+	// 	_, err := conn.Write([]byte("+PONG\r\n"))
+
+	// 	if err != nil {
+	// 		fmt.Println("Error writing to connection: ", err.Error())
+	// 		os.Exit(1)
+	// 	}
+
+	// } else {
+	// 	conn.Write([]byte("-ERR unknown command\r\n"))
+	// }
+
 }
