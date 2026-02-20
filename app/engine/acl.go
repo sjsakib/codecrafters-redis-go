29049c21d6cc157a269a4c5660d8ce879b80b20d
Sakib <sjsakib.bd@gmail.com>
2026-02-20 20:41:40 +0600

    codecrafters submit [skip ci]

--- app/engine/acl.go
+++ app/engine/acl.go
@@ -14,6 +14,17 @@ func (e *engine) handleAcl(command []string) []byte {
 	switch subcommand {
 	case "WHOAMI":
 		return resp.EncodeResp("default")
+	case "GETUSER":
+		if len(command) != 3 {
+			return resp.EncodeErrorMessage("wrong number of arguments for 'ACL GETUSER' command")
+		}
+		username := command[2]
+		if username != "default" {
+			return resp.EncodeNull()
+		}
+		return resp.EncodeResp([]any{
+			"flags", []string{},
+		})
 	default:
 		return resp.EncodeErrorMessage(fmt.Sprintf("unknown ACL subcommand '%s'", subcommand))
 	}
